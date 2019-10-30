d6d8d30d772c0af6188679c224833cf15e0298cd
Choiyunh_ <svs101@naver.com>
2019-10-30 17:53:52 +0900

    19/10/30 solved doublePQ.cpp - Programmers_Heap_4

--- uno/diskController.cpp
+++ uno/diskController.cpp
@@ -6,61 +6,53 @@
 
 using namespace std;
 
-
 bool compare(vector<int> a, vector<int> b) {
-	return a[0] < b[0];
+	if (a[0] == b[0])
+		return a[1] < b[1];
+	else
+		return a[0] < b[0];
 }
 
 struct cmp {
 	bool operator()(vector<int> a, vector<int> b) {
-		return a[1] > b[1];
+		if (a[1] == b[1])
+			return a[0] > b[0];
+		else
+			return a[1] > b[1];
 	}
 };
 
 int solution(vector<vector<int>> jobs) {
-	priority_queue<int, vector<vector<int>>, cmp> pq; // 소요시간 오름차순
-	sort(jobs.begin(), jobs.end(), compare);  // jobs 요청시점 오름차순
 	int answer = 0;
-	int time = 0;
-	vector<int> working;
 	int total = 0;
-
-	for (int i = 0; i < jobs.size(); i++)
-		pq.push(jobs[i]);
-
-	while (!pq.empty()) {
-		working = pq.top();
-
-		while (1) {
-			if (working[1] == time) {
-				if (total - working[0] < 0) {
-					total -= time;
-					time = 0;
-					vector<int> temp = working;
-					pq.pop();
-					working = pq.top();
-					pq.push(temp);
-				}
-				else {
-					answer += (total - working[0]);
-					pq.pop();
-					time = 0;
-					break;
-				}
-			}
-			time++;
-			total++;
-		}		
+	int num = jobs.size();
+	sort(jobs.begin(), jobs.end(), compare);
+	priority_queue<vector<int>,vector<vector<int>>,cmp> pq;
+	while (!jobs.empty()) {
+		pq.push(jobs.front());
+		jobs.erase(jobs.begin());
+		total = pq.top()[0] + pq.top()[1];
+		while (!jobs.empty() && total > jobs.front()[0]) { // 현재 시간보다 요청시간이 빠르면
+			pq.push(jobs.front());
+			jobs.erase(jobs.begin());
+		}
+			
+		answer += total - pq.top()[0];
+		pq.pop();
+		while (!pq.empty()) {
+			total += pq.top()[1];
+			answer += total - pq.top()[0];
+			pq.pop();
+		}
 	}
-	answer /= jobs.size();
-	return answer;
+	return answer / num;
 }
 
 int main() {
-	//vector<vector<int>> jobs = { {0,9},{0,4},{0,5},{ 0,7 },{ 0,3 } };
-	vector<vector<int>> jobs = { {0,3},{1,9},{500,6} };
-	vector<int> a;
-
+	//vector<vector<int>> jobs = { {0,9},{0,4},{0,5},{ 0,7 },{ 0,3 } }; // 답 13
+	//vector<vector<int>> jobs = { {1,9},{1,4},{1,5},{1,7},{1,3} }; // 답 13
+	//vector<vector<int>> jobs = { {0,5},{1,2},{5,5}}; // 답 6
+	vector<vector<int>> jobs = { {24,10},{18,39},{34,20},{37,5},{47,22},{20,47},{15,2},{15,34},{35,43},{26,1} };// 답74
 	printf("%d\n", solution(jobs));
 	return 0;
 }
